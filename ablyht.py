a5a38261af054ffffb057b88f0e05b640bc9222b
ABLAY ABLAY <ademmurat1616@gmail.com>
2022-06-21 23:41:24 +0300

    update

--- ablyht.py
+++ ablyht.py
@@ -2,7 +2,24 @@ banner = """
 ably Hack Tool
 Bu yazılım henüs yapım aşamasında Lütfen tam sürümü cıkarken bekleyiniz türkiye hacker adayları için mükemmel bir pakkettir
 hergün yada her hafta güncellenmektedir
+
+HTTPS://GİTHUB.COM/HACKERA316
+
+bu arac ile her tür tarama ve zafiyet taraması gerçekleştire bilirsiniz 
+bu araç içerisinde bir cok araç bulununur  bu araclardan bazıları bunlardır
+1. telefon numarası bulucu ve attak işlemi -hackera316 tarafından yapılıyor
+2. sub domain taraması web site alt alan adları taraması ve aktif olanı listeleme 
+ve bir cok tools 
+dikkat bu aracı kutu niyetle kullanmayınız kullanan olursa sorumluluk tamamen kişiye aittir 
+
+
+LICENSE Apache-2.0 license
+
+<<< BU YAZILIMI KOPYALAMAK,COĞALTMAK YADA TEKRAR DÜZENLEYİP İZİNSİZ  PAYLAŞILMASI  YASAKTIR >>>
 """
 print(banner)
 
-ipaddress = input("Lütfen Bir ip adresi giriniz #> ")
+ipaddress = input("Lütfen bir Secenek Giriniz #> ")
+
+
+
---
+++ install.sh
@@ -0,0 +1,3 @@
+echo selam kurulum yoneticisine hosgeldiniz https://github.com/HACKERA316
+
+git clone https://github.com/HACKERA316/ably-hack-tool.git
