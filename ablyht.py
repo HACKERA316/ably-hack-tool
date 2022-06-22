995efab95ce19f8b51117f950535d065b69e9cbc
ABLAY ABLAY <ademmurat1616@gmail.com>
2022-06-22 14:10:08 +0300

    Update ablyht.py

--- ablyht.py
+++ ablyht.py
@@ -1,25 +1,29 @@
-from stringcolor import *
+# banner = """
+# ably Hack Tool
+# ---------------------
+#
+# Bu yazılım henüz yapım aşamasında Lütfen tam sürümü cıkanadek bekleyiniz türkiye hacker adayları için mükemmel bir pakkettir
+# hergün yada her hafta güncellenmektedir
+#
+# HTTPS://GİTHUB.COM/HACKERA316
+#
+# bu arac ile her tür tarama ve zafiyet taraması gerçekleştire bilirsiniz
+# bu araç içerisinde bir cok araç bulununur  bu araclardan bazıları bunlardır
+# 1. telefon numarası bulucu ve attak işlemi -hackera316 tarafından yapılıyor
+# 2. sub domain taraması web site alt alan adları taraması ve aktif olanı listeleme
+# ve bir cok tools
+# dikkat bu aracı kutu niyetle kullanmayınız kullanan olursa sorumluluk tamamen kişiye aittir
+#
+#
+# LICENSE Apache-2.0 license
+#
+# <<< BU YAZILIMI KOPYALAMAK,COĞALTMAK YADA TEKRAR DÜZENLEYİP İZİNSİZ  PAYLAŞILMASI  YASAKTIR >>>
+# """
 banner = """
-ably Hack Tool
----------------------
 
-Bu yazılım henüz yapım aşamasında Lütfen tam sürümü cıkanadek bekleyiniz türkiye hacker adayları için mükemmel bir pakkettir
-hergün yada her hafta güncellenmektedir
-
-HTTPS://GİTHUB.COM/HACKERA316
-
-bu arac ile her tür tarama ve zafiyet taraması gerçekleştire bilirsiniz 
-bu araç içerisinde bir cok araç bulununur  bu araclardan bazıları bunlardır
-1. telefon numarası bulucu ve attak işlemi -hackera316 tarafından yapılıyor
-2. sub domain taraması web site alt alan adları taraması ve aktif olanı listeleme 
-ve bir cok tools 
-dikkat bu aracı kutu niyetle kullanmayınız kullanan olursa sorumluluk tamamen kişiye aittir 
-
-
-LICENSE Apache-2.0 license
-
-<<< BU YAZILIMI KOPYALAMAK,COĞALTMAK YADA TEKRAR DÜZENLEYİP İZİNSİZ  PAYLAŞILMASI  YASAKTIR >>>
 """
-print(banner)
 
-print(cs("wild", "pink")+" stuff")
+
+
+
+print(banner)
